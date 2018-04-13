56ee12fdeb75b6a35942909f6a037d717c2986ff
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-04-14 00:39:27 +0200

    Initial Commit

---
+++ src/InvalidReactionTypeException.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Aaronmacaron\Reaction;
+
+
+use Exception;
+use Throwable;
+
+class InvalidReactionTypeException extends Exception
+{
+    /** @var $type int */
+    private $type;
+
+    /** @var $reaction Reaction */
+    private $reaction;
+
+    /**
+     * InvalidReactionTypeException constructor.
+     * @param int $type
+     * @param Reaction $reaction
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(
+        int $type,
+        Reaction $reaction = null,
+        string $message = null,
+        int $code = 0,
+        Throwable $previous = null
+    )
+    {
+        if ($message === null) {
+            $message = "${type} is not a valid reaction type value.";
+        }
+
+        parent::__construct($message, $code, $previous);
+
+        $this->type = $type;
+        $this->reaction = $reaction;
+    }
+
+    /**
+     * Getter for $this->type
+     *
+     * @return int
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * Getter for $this->reaction
+     *
+     * @return Reaction
+     */
+    public function getReaction(): Reaction
+    {
+        return $this->reaction;
+    }
+
+}
---
+++ src/Reaction.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Aaronmacaron\Reaction;
+
+/**
+ * Class Reaction
+ * A Reaction represents a response object of any action. A Reaction can be succeed or fail.
+ * You should use Reactions when a negative response is expected and you want to provide a message to the response.
+ *
+ * @package Aaronmacaron\Reaction
+ */
+class Reaction
+{
+    /** @var int */
+    const SUCCESS = 1;
+
+    /** @var int */
+    const FAILURE = 2;
+
+    /** @var $message string */
+    private $message;
+
+    /** @var $type int */
+    private $type;
+
+    /**
+     * Reaction constructor.
+     *
+     * @param $message
+     * @param $type
+     * @throws InvalidReactionTypeException
+     */
+    public function __construct(string $message, int $type)
+    {
+        $this->message = $message;
+
+        if (!$this->isValidType($type)) {
+            throw new InvalidReactionTypeException($type, $this);
+        }
+        $this->type = $type;
+    }
+
+    /**
+     * This method registers an executor that will be executed on success.
+     *
+     * @param callable $executor The executor is executed on success.
+     * @return Reaction The same Reaction that is used for method chaining.
+     */
+    public function succeed(callable $executor): Reaction
+    {
+        if ($this->isSuccess()) {
+            $executor($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * This method registers an executor that will be executed on failure.
+     *
+     * @param callable $executor The executor is executed on failure.
+     * @return Reaction The same Reaction that is used for method chaining.
+     */
+    public function fail(callable $executor): Reaction
+    {
+        if ($this->isFailure()) {
+            $executor($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Checks if reaction is success.
+     *
+     * @return bool Returns true if reaction is of type success.
+     */
+    public function isSuccess(): bool
+    {
+        return $this->type === Reaction::SUCCESS;
+    }
+
+    /**
+     * Checks if reaction is failure.
+     *
+     * @return bool Returns true if reaction is of type failure.
+     */
+    public function isFailure(): bool
+    {
+        return $this->type === Reaction::FAILURE;
+    }
+
+    /**
+     * Generates new success reaction.
+     *
+     * @param string $message The message of the reaction
+     * @return Reaction The newly generated reaction.
+     * @throws InvalidReactionTypeException
+     */
+    public static function success($message = ''): Reaction
+    {
+        return new Reaction($message, Reaction::SUCCESS);
+    }
+
+    /**
+     * Generates new failure reaction.
+     *
+     * @param string $message The message of the reaction
+     * @return Reaction The newly generated reaction.
+     * @throws InvalidReactionTypeException
+     */
+    public static function failure($message = ''): Reaction
+    {
+        return new Reaction($message, Reaction::FAILURE);
+    }
+
+    /**
+     * Checks if integer is a valid reaction type.
+     *
+     * @param int $type The type to check.
+     * @return bool True if type is valid.
+     */
+    private function isValidType(int $type): bool
+    {
+        $validTypes = [
+            Reaction::SUCCESS,
+            Reaction::FAILURE,
+        ];
+
+        return in_array($type, $validTypes);
+    }
+
+}
